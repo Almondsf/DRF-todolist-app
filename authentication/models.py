a865997a60de8b737b41d07326be467bc229ed8f
Oluwafemi Oloyede <oloyedeoluwafemi22@gmail.com>
2024-12-03 16:54:42 +0100

    Authenticate a user. Get JWT Access Token.

--- authentication/models.py
+++ authentication/models.py
@@ -6,6 +6,9 @@ from django.contrib.auth.validators import UnicodeUsernameValidator
 from django.utils.translation import gettext_lazy as _
 from django.utils import timezone
 from django.contrib.auth.hashers import make_password
+import jwt
+from django.conf import settings
+from datetime import datetime, timedelta
 
 # Create your models here.
 
@@ -92,4 +95,6 @@ class User(PermissionsMixin, AbstractBaseUser, TrackingModel):
     
     @property
     def token(self):
-        return ''
+        token=jwt.encode({'username':self.username, 'email':self.email, 'exp':datetime.utcnow() + timedelta(hours=24)}, settings.SECRET_KEY, algorithm='HS256')
+        
+        return token
